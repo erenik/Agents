6d5a9bea345d74b3d129c4360ea72c7a0b98369d
Ubuntu <ubuntu@ip-172-31-24-163.us-west-2.compute.internal>
2016-10-17 12:02:30 +0000

    Startup script now compiles and runs server agent

--- AgentSmith/Startup.sh
+++ AgentSmith/Startup.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 echo Startup script running... > AgentStartup.log
-java jade.Boot HostAgent:AgentSmith > AgentStartup.log
+javac ServerAgentSmith.java
+java jade.Boot -agents HostAgent:ServerAgentSmith > AgentStartup.log
