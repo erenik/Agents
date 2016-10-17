25ade7082e75d25757edf9f1c8bb61733a8695df
Emil Hedemalm <emil_hedemalm@hotmail.com>
2016-10-17 15:05:52 +0200

    Updated startup script slightly

--- AgentSmith/Startup.sh
+++ AgentSmith/Startup.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-echo Startup script running... > AgentStartup.log
-git pull
+echo Startup script running... > AgentStartupScript.log
 agentDir=/home/ubuntu/JADE/Agents/AgentSmith
+cd $agentDir
+echo Yolo > Startup.sh.log
+git pull
 javac $agentDir/ServerAgentSmith.java
-java jade.Boot -agents HostAgent:$agentDir/ServerAgentSmith > AgentStartup.log
+java jade.Boot -agents hostAgent:ServerAgentSmith & 
