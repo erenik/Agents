df35d3a08731b881e145746e5f91ceb16a9966b9
Ubuntu <ubuntu@ip-172-31-24-163.us-west-2.compute.internal>
2016-10-17 12:09:44 +0000

    Added git pull to startup script, so it is updated as needed each reboot

--- AgentSmith/Startup.sh
+++ AgentSmith/Startup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 echo Startup script running... > AgentStartup.log
+git pull
 agentDir=/home/ubuntu/JADE/Agents/AgentSmith
 javac $agentDir/ServerAgentSmith.java
 java jade.Boot -agents HostAgent:$agentDir/ServerAgentSmith > AgentStartup.log
