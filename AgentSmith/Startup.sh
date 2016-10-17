d8c7b04b6111d2eab0c64897eaa8a009a67282f3
Ubuntu <ubuntu@ip-172-31-24-163.us-west-2.compute.internal>
2016-10-17 12:05:49 +0000

    Absolute paths

--- AgentSmith/Startup.sh
+++ AgentSmith/Startup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 echo Startup script running... > AgentStartup.log
-javac ServerAgentSmith.java
-java jade.Boot -agents HostAgent:ServerAgentSmith > AgentStartup.log
+agentDir=/home/ubuntu/JADE/Agents/AgentSmith
+javac $agentDir/ServerAgentSmith.java
+java jade.Boot -agents HostAgent:$agentDir/ServerAgentSmith > AgentStartup.log
