02ec183390ca6bc625f00f630278151e7c33774b
Emil Hedemalm <emil_hedemalm@hotmail.com>
2016-10-18 19:12:44 +0200

    Volumetric-interested Architects and agents added

---
+++ TCPAgent/ArchitectVolume.java
@@ -0,0 +1,179 @@
+import jade.core.Agent;
+import jade.core.*;
+import jade.core.behaviours.*;
+import jade.lang.acl.ACLMessage;
+import jade.gui.*;
+import java.lang.Long;
+
+import jade.domain.AMSService;
+import jade.domain.FIPAAgentManagement.AMSAgentDescription;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
+import java.awt.event.*;
+import java.util.ArrayList;
+import jade.domain.*;
+import jade.domain.FIPAAgentManagement.*;
+import jade.lang.acl.*;
+import jade.content.*;
+import jade.content.lang.*;
+import jade.content.lang.sl.*;
+import jade.lang.acl.ACLMessage;
+import jade.content.onto.*;
+import jade.content.onto.basic.*;
+import jade.util.leap.*;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+
+import jade.wrapper.ContainerController;
+import jade.wrapper.AgentContainer;
+import jade.wrapper.StaleProxyException;
+import jade.wrapper.*;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JList;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+
+
+public class ArchitectVolume extends Agent{
+	
+	public GUI gui;
+	public int agentEnumerator = 0;
+	
+	// setup agent
+	protected void setup() 
+	{
+		
+		// some nice quote
+        System.out.println("'I'm here to destroy servers and chew bubble gum, and i'm all out of bubble gum.' - Architect " + getLocalName());
+        // initializes the gui
+        gui = new GUI();
+        // associates the gui
+        //gui.setAgent(this);
+        
+        /** Registration with the DF */
+        DFAgentDescription dfd = new DFAgentDescription();
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType("ArchitectVolume");
+        sd.setName(getName());
+        sd.setOwnership("The Architect itself");
+        sd.addOntologies("ArchitectVolume");
+        dfd.setName(getAID());
+        dfd.addServices(sd);
+        try {
+			DFService.register(this,dfd);
+        } catch (FIPAException e) {
+			System.err.println(getLocalName()+" registration with DF unsucceeded. Reason: "+e.getMessage());
+			doDelete();
+        }  
+        
+	}
+
+}
+
+
+class GUI extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField ip;
+	private JTextField port;
+	private JTextField content;
+	private JTextField period;
+
+
+	/**
+	 * Create the frame.
+	 */
+	public GUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		setTitle("Architect - Create soldier that spew volumes of data");
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblIP = new JLabel("IP");
+		lblIP.setBounds(40, 43, 175, 14);
+		contentPane.add(lblIP);
+		
+		JLabel lblPort = new JLabel("Port");
+		lblPort.setBounds(40, 79, 175, 14);
+		contentPane.add(lblPort);
+		
+		JLabel lblContent = new JLabel("Content (numBytes)");
+		lblContent.setBounds(40, 120, 175, 14);
+		contentPane.add(lblContent);
+		
+		JLabel lblPeriod = new JLabel("Periodicity (ms)");
+		lblPeriod.setBounds(40, 164, 175, 14);
+		contentPane.add(lblPeriod);
+		
+		ip = new JTextField();
+		ip.setBounds(225, 40, 199, 20);
+		ip.setText("tcp-368140612.us-west-2.elb.amazonaws.com");
+		contentPane.add(ip);
+		ip.setColumns(10);
+		
+		port = new JTextField();
+		port.setBounds(225, 76, 199, 20);
+		port.setText("4032");
+		contentPane.add(port);
+		port.setColumns(10);
+		
+		content = new JTextField();
+		content.setBounds(225, 117, 199, 20);
+		content.setText("1000");
+		contentPane.add(content);
+		content.setColumns(10);
+		
+		period = new JTextField();
+		period.setBounds(225, 161, 199, 20);
+		period.setText("1");
+		contentPane.add(period);
+		period.setColumns(10);
+		
+		JButton btnCreate = new JButton("Create Agent");
+		btnCreate.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				// getting data
+				Object[] args = { ip.getText(), port.getText(),content.getText(), period.getText()}; 
+				
+				//Get the JADE runtime interface (singleton)
+				jade.core.Runtime runtime = jade.core.Runtime.instance();
+				//Create a Profile, where the launch arguments are stored
+				Profile profile = new ProfileImpl();
+				profile.setParameter(Profile.CONTAINER_NAME, "Attack");
+				profile.setParameter(Profile.MAIN_HOST, "localhost");
+				//create a non-main agent container
+				ContainerController container = runtime.createAgentContainer(profile);
+				try {
+						String name = "Agent "+String.valueOf(++agentEnumerator);
+				        AgentController ag = container.createNewAgent(name, 
+				                                      "TCPAgentVolume", 
+				                                      args);//arguments
+				        ag.start();
+				} catch (StaleProxyException e) {
+				    e.printStackTrace();
+				}
+			}
+		});
+		btnCreate.setBounds(146, 211, 157, 23);
+		contentPane.add(btnCreate);
+		
+		setVisible(true);
+	}
+}
---
+++ TCPAgent/TCPAgentVolume.java
@@ -0,0 +1,194 @@
+import jade.core.Agent;
+import jade.core.*;
+import jade.core.behaviours.*;
+import jade.lang.acl.ACLMessage;
+import jade.gui.*;
+import java.lang.Long;
+import java.net.Socket;
+
+import jade.domain.AMSService;
+import jade.domain.FIPAAgentManagement.AMSAgentDescription;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
+import java.awt.event.*;
+import java.util.ArrayList;
+import jade.domain.*;
+import jade.domain.FIPAAgentManagement.*;
+import jade.lang.acl.*;
+import jade.content.*;
+import jade.content.lang.*;
+import jade.content.lang.sl.*;
+import jade.lang.acl.ACLMessage;
+import jade.content.onto.*;
+import jade.content.onto.basic.*;
+import jade.util.leap.*;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+
+import java.util.Iterator;
+import java.util.Date;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.OutputStreamWriter;
+import java.lang.Exception;
+import java.lang.Math;
+
+
+
+public class TCPAgentVolume extends Agent {
+	
+	private String ip;
+	private int port;
+	private String content;
+	private int period;
+	
+	// setup agent
+	protected void setup() 
+	{
+		Object[] args = getArguments();
+		ip = args[0].toString();
+		port = Integer.valueOf(args[1].toString());
+		content = args[2].toString();
+		period = Integer.valueOf(args[3].toString());
+		// Parse into integer.
+		int length = Integer.parseInt(content);
+		char[] cArr = new char[length];
+		content = new String(cArr);
+	
+		
+		// some nice quote
+        System.out.println("I'm here to spam some TCP server - " + getLocalName());
+        
+        /** Registration with the DF */
+        DFAgentDescription dfd = new DFAgentDescription();
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType("TCPAgentVolume");
+        sd.setName(getName());
+        sd.setOwnership("Val&Emil");
+        sd.addOntologies("TCPAgentVolume");
+        dfd.setName(getAID());
+        dfd.addServices(sd);
+        try {
+			DFService.register(this,dfd);
+        } catch (FIPAException e) {
+			System.err.println(getLocalName()+" registration with DF unsucceeded. Reason: "+e.getMessage());
+			doDelete();
+        }  
+        
+        addBehaviour(new CyclicBehaviour(this){
+        	public void action(){
+        		// we send a message every second to a tcp server        		
+        		try {
+        			SendMessage th = new SendMessage(ip, port, content);
+        			th.run();
+        			Thread.sleep(period);
+        			
+        		} catch (Exception e) {
+        			System.out.println("error "+e.getMessage());
+        		}
+        	}
+        });
+    } // end setup
+	
+
+}
+
+class SendMessage implements Runnable {
+	
+	private String ip;
+	private int port;
+	private String msg;
+	private Socket s;
+	private int id;
+	
+	public SendMessage(String nIp, int nPort, String nMsg) {
+		ip = nIp;
+		port = nPort;
+		msg = nMsg;
+		id = (int) (Math.random()*1000.0);
+	}
+	
+	public void run() {
+		try {
+			System.out.println(id+":trying to connect...");
+    		Socket s = new Socket(ip, port);
+    		System.out.println(id+":Connected.");
+    		DataInputStream in = new DataInputStream( s.getInputStream());
+    		DataOutputStream out = new DataOutputStream( s.getOutputStream());
+    		
+    		out.writeBytes(msg);
+    		out.flush();
+    		System.out.println(id+":"+msg+" Sent.");
+    		@SuppressWarnings("deprecation")
+			String line = in.readLine();
+    		System.out.println(id+":Received:"+line);
+    		s.close();
+		} catch (Exception e) {
+			System.out.println(id+":error "+e.getMessage());
+		}
+	}
+}
+
+	
+
+				
+/*		JButton btnSendThreat = new JButton("Send Threat");
+		btnSendThreat.setBounds(10, 136, 205, 23);
+		contentPane.add(btnSendThreat);
+		btnSendThreat.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0)
+			{
+				// Send a default message for now.
+				ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
+				msg.addReceiver(new AID(receiver.getItemAt(receiver.getSelectedIndex()), AID.ISLOCALNAME));
+				msg.setLanguage("English");
+				msg.setContent("Who are you?");
+				myAgent.send(msg);
+				System.out.println("Sent message");  
+			}	
+		});
+
+
+class SendMessage extends OneShotBehaviour {
+
+    public void action() {
+        /*ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
+        msg.addReceiver(new AID("R", AID.ISLOCALNAME));
+        msg.setLanguage("English");
+        msg.setContent("Hello How Are You?");
+        send(msg);
+        System.out.println("****I Sent Message to::> R1 *****"+"\n"+
+                            "The Content of My Message is::>"+ msg.getContent());
+    }
+}
+
+class ReceiveMessage extends CyclicBehaviour {
+
+    // Variable to Hold the content of the received Message
+    private String Message_Performative;
+    private String Message_Content;
+    private String SenderName;
+    private String MyPlan;
+
+
+    public void action() {
+        /*ACLMessage msg = receive();
+        if(msg != null) {
+
+            Message_Performative = msg.getPerformative(msg.getPerformative());
+            Message_Content = msg.getContent();
+            SenderName = msg.getSender().getLocalName();
+            System.out.println(" ****I Received a Message***" +"\n"+
+                    "The Sender Name is::>"+ SenderName+"\n"+
+                    "The Content of the Message is::> " + Message_Content + "\n"+
+                    "::: And Performative is::> " + Message_Performative + "\n");
+            System.out.println("ooooooooooooooooooooooooooooooooooooooo");
+
+        }
+
+    }
+*/
