99dbdac7628ea69928dec7b38b0adcb52a9bf126
Emil Hedemalm <emil_hedemalm@hotmail.com>
2016-10-24 12:47:28 +0200

    fixes

--- Creature/Creature.java
+++ Creature/Creature.java
@@ -6,8 +6,8 @@ public class Creature // 12-Complete the Class definition
 {
 	protected void setup()
 	{
-		addBehaviour((// 13-Create a behavior from an appropriate
-			new TickerBehaviour(this, 200)
+		addBehaviour( 
+			new TickerBehaviour(this, 200) // 13-Create a behavior from an appropriate
 			{
 				public void action()
 				{
@@ -15,7 +15,7 @@ public class Creature // 12-Complete the Class definition
 					if (msg!=null) 
 					{
 						System.out.println( " - " + myAgent.getLocalName() + " received: " + msg.getContent() );
-						ACLMessage reply = new ACLMessage(ACLMessage.INFORM); // 14-create an ACL message called “reply” // 15-set its performative to INFORM
+						ACLMessage reply = new ACLMessage(ACLMessage.INFORM); // 14-create an ACL message called "reply" // 15-set its performative to INFORM
 						reply.setContent("Hi Yourself");// 16-set appropriate message content as mentioned in the question
 						reply.addReceiver(msg.getSender());
 						this.send(reply); // 17-send the message to the sender
--- Creature/CreatureAgent.java
+++ Creature/CreatureAgent.java
@@ -1,13 +1,13 @@
 /**
 	1- A multi-agent system consists of 2 Agent Types, CreatorAgent and Creature. When you
-	run an instance of CreatoreAgent (in this case “CreatorAgent#1”), it will do the
+	run an instance of CreatoreAgent (in this case "CreatorAgent#1"), it will do the
 	followings:
 	a- Creates an Instance of Creature and names it Bender-The-Offender.
 	b- Runs the Bender-The-Offender in the same container as itself.
-	c- Sends a message to Bender-The-Offender with Performative “INFORM” and
-	content “Hi”
-	Bender-The-Offender, in return replies any message by Performative “INFORM” and
-	content “Hi Yourself”
+	c- Sends a message to Bender-The-Offender with Performative "INFORM" and
+	content "Hi"
+	Bender-The-Offender, in return replies any message by Performative INFORM and
+	content "Hi Yourself"
 	The contents of the CreatureAgent.java and Creature.java are listed below, but the
 	codes are incomplete. Complete the codes whenever is required, so that the above
 	scenario can be executed by these two Agents. In total 17 lines of modifications are
@@ -17,8 +17,8 @@
 import jade.core.Agent;
 import jade.core.behaviours.*;
 import jade.core.AID;
-import jade.wrapper.AgentContainer; // 1- Ipmort Appropriate Jade’s Class
-import jade.wrapper. // 2- Import Appropriate Jade’s Class
+import jade.wrapper.AgentContainer; // 1- Ipmort Appropriate Jade's Class
+// import jade.wrapper. // 2- Import Appropriate Jade's Class
 import jade.lang.acl.*;
 
 public class CreatureAgent extends Agent //3- Complete the Class definition 
@@ -27,7 +27,7 @@ public class CreatureAgent extends Agent //3- Complete the Class definition
 	AID Bender = new AID("Creature", ISLOCALNAME);// 4-Complete the code to Instantiate this Object from AID class
 	protected void setup()
 	{
-		AgentContainer c = getContainerController(); // 5-Get the name of current container and assign it to “c”
+		AgentContainer c = getContainerController(); // 5-Get the name of current container and assign it to "c"
 		try {
 			AgentController a = c.createNewAgent(name, "Creature", null); // 6-Create Bender-The-Offender in the current container
 			ag.start(); // 7-Start your agent
@@ -43,13 +43,13 @@ public class CreatureAgent extends Agent //3- Complete the Class definition
 					ACLMessage msg = new
 					ACLMessage(ACLMessage.INFORM);
 					msg.setContent("Hi" );
-					msg.addReceiver(new AID(name, AID.ISLOCALNAME); //9- Add the Receiver
+					msg.addReceiver(new AID(name, AID.ISLOCALNAME)); //9- Add the Receiver
 					System.out.println("+++ Sending: " + n);
 					this.send(msg);	//10- Send the Message
 					block( 1000 );
 				}
 				public boolean done() {
-					return ++n <= 3; // 11-use variable ”n” to run this Behaviour 3 times 
+					return ++n <= 3; // 11-use variable "n" to run this Behaviour 3 times 
 				}
 			}
 		);
